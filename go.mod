8a327da663fdedc69c2716cdfb86a833da6ebf3a
Johannes Gehrs <johannes.gehrs@moia.io>
2021-12-15 09:25:28 +0100

    WIP

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/johannes-gehrs/goava
 
 go 1.18
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/armon/go-radix v1.0.0
+	github.com/stretchr/testify v1.7.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- goava.go
+++ goava.go
@@ -1,23 +1,56 @@
 package goava
 
+import (
+	"sync"
+	"time"
+
+	"github.com/armon/go-radix"
+)
+
 type (
+	item[ValT comparable] struct {
+		expiresAt time.Time
+		val       ValT
+	}
+
 	Cache[KeyT, ValT comparable] struct {
-		cm map[KeyT]ValT
+		cm                map[KeyT]item[ValT]
+		itemsByExpiration *radix.Tree
+		mutex             *sync.Mutex
 	}
 )
 
+func (it *item[ValT]) isExpired(now time.Time) bool {
+	return !(it.expiresAt.After(now))
+}
+
 func (c *Cache[KeyT, ValT]) Get(key KeyT) (ValT, bool) {
-	val, found := c.cm[key]
-	return val, found
+	var zeroVal ValT
+
+	item, found := c.cm[key]
+	if !found || item.isExpired(time.Now()) {
+		return zeroVal, false
+	}
+
+	return item.val, found
 }
 
-func (c *Cache[KeyT, ValT]) Set(key KeyT, val ValT) {
-	c.cm[key] = val
+func (c *Cache[KeyT, ValT]) Set(key KeyT, val ValT, expiresAt time.Time) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.cm[key] = item[ValT]{
+		expiresAt: expiresAt,
+		val:       val,
+	}
+	c.itemsByExpiration.Insert(expiresAt.UTC().Format(time.RFC3339Nano), key)
 }
 
-func NewCache[KeyT, ValT comparable]() *Cache[KeyT, ValT] {
+func NewCache[KeyT, ValT comparable](maxItems, maxEvictionsPerSecond int64) *Cache[KeyT, ValT] {
 	c := &Cache[KeyT, ValT]{
-		cm: make(map[KeyT]ValT),
+		cm:                make(map[KeyT]item[ValT]),
+		mutex:             &sync.Mutex{},
+		itemsByExpiration: radix.New(),
 	}
 
 	return c
