a205e16ed5c0797bda94b1ee9f2be1426a30c1b8
Johannes Gehrs <johannes.gehrs@moia.io>
2021-12-16 17:27:52 +0100

    WIP

--- goava.go
+++ goava.go
@@ -1,6 +1,8 @@
 package goava
 
 import (
+	"context"
+	"fmt"
 	"sync"
 	"time"
 
@@ -13,45 +15,134 @@ type (
 		val       ValT
 	}
 
-	Cache[KeyT, ValT comparable] struct {
-		cm                map[KeyT]item[ValT]
-		itemsByExpiration *radix.Tree
-		mutex             *sync.Mutex
-		cleanupOnlyOnce   *sync.Once
+	Cache[KeyT comparable, ValT any] struct {
+		cm                                 map[KeyT]item[ValT]
+		itemsByExpiration                  *radix.Tree
+		mutex                              *sync.Mutex
+		onlyOneEvictionProcessAtAGivenTime *sync.Once
 	}
+
 )
 
-func (it *item[ValT]) isExpired(now time.Time) bool {
-	return !(it.expiresAt.After(now))
+func isExpired(now, expiryTime time.Time) bool {
+	return !(expiryTime.After(now))
+}
+
+func (it *item[ValT]) expiryTimeAsString() string {
+	return it.expiresAt.UTC().Format(time.RFC3339Nano)
+}
+
+func expiryTimeFromString(s string) time.Time {
+	t, err := time.Parse(time.RFC3339Nano, s)
+	if err != nil {
+		panic(fmt.Sprintf("unable to parse time in tree-datastructure as time.Time - this is unexpected. String is '%s'",
+			s))
+	}
+
+	return t
 }
 
 func (c *Cache[KeyT, ValT]) Get(key KeyT) (ValT, bool) {
 	var zeroVal ValT
 
 	item, found := c.cm[key]
-	if !found || item.isExpired(time.Now()) {
+	if !found || isExpired(time.Now(), item.expiresAt) {
 		return zeroVal, false
 	}
 
 	return item.val, found
 }
 
-func (c *Cache[KeyT, ValT]) Set(key KeyT, val ValT, expiresAt time.Time) {
+func (c *Cache[KeyT, ValT]) Put(key KeyT, val ValT, expiresAt time.Time) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
-	c.cm[key] = item[ValT]{
+	itm := item[ValT]{
 		expiresAt: expiresAt,
 		val:       val,
 	}
-	c.itemsByExpiration.Insert(expiresAt.UTC().Format(time.RFC3339Nano), key)
+
+	c.cm[key] = itm
+
+	treeValues, found := c.itemsByExpiration.Get(itm.expiryTimeAsString())
+	if !found {
+		treeValues = make(map[KeyT]struct{})
+	}
+
+	treeValues.(map[KeyT]struct{})[key] = struct {}{}
+
+	c.itemsByExpiration.Insert(itm.expiryTimeAsString(), treeValues)
 }
 
-func NewCache[KeyT comparable, ValT any](maxItems, maxEvictionsPerSecond int64) *Cache[KeyT, ValT] {
+func (c *Cache[KeyT, ValT]) Delete(key KeyT) bool {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	itm, found := c.cm[key]
+	if !found {
+		return false
+	}
+
+	delete(c.cm, key)
+
+	treeValues, found := c.itemsByExpiration.Get(itm.expiryTimeAsString())
+	if !found {
+		treeValues = make(map[KeyT]struct{})
+	}
+
+	treeValuesTypeAsserted := treeValues.(map[KeyT]struct{})
+
+	delete(treeValuesTypeAsserted, key)
+
+	if len(treeValuesTypeAsserted) == 0 {
+		c.itemsByExpiration.Delete(itm.expiryTimeAsString())
+	} else {
+		c.itemsByExpiration.Insert(itm.expiryTimeAsString(), treeValuesTypeAsserted)
+	}
+
+	return true
+}
+
+func (c *Cache[KeyT, ValT]) EvictExpiredKeys(ctx context.Context) (evictedCount int64) {
+	c.onlyOneEvictionProcessAtAGivenTime.Do(func() {
+		c.mutex.Lock()
+		toBeEvicted := make([]KeyT, 0)
+		c.itemsByExpiration.Walk(func(s string, cacheKeyMap interface{}) bool {
+			select {
+			case <-ctx.Done():
+				// timeout reached - we'll stop walking the tree but still remove the items we collected
+				return true
+			default:
+				if isExpired(time.Now(), expiryTimeFromString(s)) {
+					for cacheKey := range cacheKeyMap.(map[KeyT]struct{}) {
+						toBeEvicted = append(toBeEvicted, cacheKey)
+					}
+					return false
+				}
+				// if the key isn't expired all the next ones also won't be expired as they are sorted by eviction time in the tree.
+				// So we can stop iteration.
+				return true
+			}
+		})
+		c.mutex.Unlock()
+
+		for _, cacheKey := range toBeEvicted {
+			c.Delete(cacheKey)
+			evictedCount++
+		}
+	})
+
+	c.onlyOneEvictionProcessAtAGivenTime = &sync.Once{}
+
+	return evictedCount
+}
+
+func NewCache[KeyT comparable, ValT any]() *Cache[KeyT, ValT] {
 	c := &Cache[KeyT, ValT]{
-		cm:                make(map[KeyT]item[ValT]),
-		mutex:             &sync.Mutex{},
-		itemsByExpiration: radix.New(),
+		cm:                                 make(map[KeyT]item[ValT]),
+		mutex:                              &sync.Mutex{},
+		itemsByExpiration:                  radix.New(),
+		onlyOneEvictionProcessAtAGivenTime: &sync.Once{},
 	}
 
 	return c
--- goava_test.go
+++ goava_test.go
@@ -1,17 +1,65 @@
 package goava
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCache(t *testing.T) {
+func TestCachePutGetAndDelete(t *testing.T) {
 	c := NewCache[string, int64]()
 
-	c.Set("johannes", 16)
+	c.Put("johannes", 16, time.Now().Add(time.Hour))
 
 	res, found := c.Get("johannes")
 	assert.True(t, found)
 	assert.EqualValues(t, 16, res)
+
+	assert.Equal(t, 1, c.itemsByExpiration.Len())
+
+	found = c.Delete("johannes")
+	assert.True(t, found)
+
+	assert.Equal(t, 0, c.itemsByExpiration.Len())
+
+	_, found = c.Get("johannes")
+	assert.False(t, found)
 }
+
+func TestCacheEvictExpiredKey(t *testing.T) {
+	c := NewCache[string, int64]()
+
+	for i := 0; i < 10*1000; i++ {
+		c.Put(fmt.Sprintf("%016d", i), int64(i), time.Now().Add(time.Hour).Add(time.Second*time.Duration(i)))
+	}
+
+	for i := 10 * 1000; i < 20*1000; i++ {
+		c.Put(fmt.Sprintf("%016d", i), int64(i), time.Now().Add(-time.Hour).Add(-(time.Second * time.Duration(i))))
+	}
+
+	evicted := c.EvictExpiredKeys(context.TODO())
+
+	assert.EqualValues(t, 10 * 1000, evicted)
+	assert.Equal(t, 10 * 1000, c.itemsByExpiration.Len())
+}
+
+func TestCacheIdenticalExpirationTimes(t *testing.T) {
+	c := NewCache[string, int64]()
+
+	for i := 0; i < 10*1000; i++ {
+		c.Put(fmt.Sprintf("%016d", i), int64(i), time.Now().Add(time.Hour))
+	}
+
+	for i := 10 * 1000; i < 20*1000; i++ {
+		c.Put(fmt.Sprintf("%016d", i), int64(i), time.Now().Add(-time.Hour).Add)
+	}
+
+	evicted := c.EvictExpiredKeys(context.TODO())
+
+	assert.EqualValues(t, 1, evicted)
+	assert.Equal(t, 10 * 1000, c.itemsByExpiration.Len())
+}
+
